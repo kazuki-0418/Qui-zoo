a28671952434697de9ffb63458f5f085c44bd452
alexchris99 <cajt@outloo.com>
2025-04-15 13:43:55 -0700

    Middleware for userActivityLog added

--- backend/src/routes/userActivityLog.routes.ts
+++ backend/src/routes/userActivityLog.routes.ts
@@ -1,7 +1,12 @@
 import { Router } from "express";
 import { LogController } from "../controllers/userActivityLog.controller";
+import { auth, sessionExist } from "../middleware/auth.middleware";
 
 const activityLogRouter = Router();
+
+// Middleware
+activityLogRouter.use(sessionExist);
+activityLogRouter.use(auth);
 
 // create userActivityLog
 activityLogRouter.post("/create", LogController.createUserActivityLog);
