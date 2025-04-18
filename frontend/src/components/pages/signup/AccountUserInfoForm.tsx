123025bae8f640ed2a273d8edd022ab4b11898a4
Ryoga <isimaruru.1712@gmail.com>
2025-04-17 19:25:50 -0700

    Adjust btn of AccountUserInfoSection and BasicInfoSection

--- frontend/src/components/pages/signup/AccountUserInfoForm.tsx
+++ frontend/src/components/pages/signup/AccountUserInfoForm.tsx
@@ -79,7 +79,7 @@ export function AccountUserInfoForm({
         )}
       </div>
 
-      <div className="flex justify-between flex-col sm:flex-row">
+      <div className="flex justify-between flex-col sm:flex-row  gap-6">
         <AuthButton type="button" variant="secondary" onClick={onBack}>
           Back
         </AuthButton>
