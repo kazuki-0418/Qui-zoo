12618703a5e5c4422ac9223eb067d57e8e684a7f
kazuki-0418 <kazuki.castle0418@gmail.com>
2025-04-22 04:35:33 -0700

    deploy error

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,9 +2,13 @@ import withFlowbiteReact from "flowbite-react/plugin/nextjs";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  images: {
-    domains: ["btxvsdoiswwlufmfghlz.supabase.co"],
+  // 既存の設定
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      lightningcss: false,
+    };
+    return config;
   },
 };
 
